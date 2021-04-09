ee1520a534dbd8ec5df8fe5decf4bad202d37fe6
Ryan Ha <ha819ha.dev@gmail.com>
2021-04-09 15:13:13 +0900

    [#10] AWS RDS 연결

--- src/config/databse.ts
+++ src/config/databse.ts
@@ -9,11 +9,26 @@ export interface DatabaseConfig {
   database: string;
 }
 
-export default registerAs('database', () => ({
-  type: process.env.DB_DIALECT,
+const IS_OFFLINE = process.env.IS_OFFLINE;
+
+const localDBConfig: DatabaseConfig = {
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+};
+
+const remoteDBConfig: DatabaseConfig = {
+  type: process.env.DB_DIALECT as 'mysql',
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT, 10) || 3306,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-}));
+};
+
+export default registerAs('database', () =>
+  IS_OFFLINE !== 'false' ? localDBConfig : remoteDBConfig,
+);
